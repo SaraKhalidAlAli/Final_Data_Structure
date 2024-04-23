9b141d7f8204c9cf2a083c13f8c43a1063dcdcdf
SaraKhalidAlAli <202206310@zu.ac.ae>
2024-04-24 00:22:23 +0400

    starting test cases

--- PartA_Task3.py
+++ PartA_Task3.py
@@ -16,3 +16,26 @@ class MaxHeap:
             return None
 
 
+#Example:
+
+#TEAST CASE 1:creating a Max Heap
+if __name__ == "__main__":
+    # Create a Max Heap
+    max_heap = MaxHeap()
+
+    # Inserting posts with view counts and the name of the poster
+    max_heap.insert({"content": "Post 1", "views": 100, "poster": "User A"})
+    max_heap.insert({"content": "Post 2", "views": 200, "poster": "User B"})
+    max_heap.insert({"content": "Post 3", "views": 150, "poster": "User C"})
+    max_heap.insert({"content": "Post 4", "views": 300, "poster": "User D"})
+    max_heap.insert({"content": "Post 5", "views": 250, "poster": "User E"})
+
+    # Retrieve the post with the highest number of views
+    max_views_post = max_heap.get_max_views_post()
+    print("\n------------------------------------------------------------------------------------------------")
+    if max_views_post:
+        print(f"The post with the highest number of views is: {max_views_post['content']} with {max_views_post['views']} views, posted by {max_views_post['poster']}.")
+    else:
+        print("No posts available.")
+
+print("\n------------------------------------------------------------------------------------------------")
