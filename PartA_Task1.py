a5fdaf1a748f40a2d82388ca39963bca592411bb
SaraKhalidAlAli <202206310@zu.ac.ae>
2024-04-23 21:42:59 +0400

    method to imsert key value pair into the hack table we have here ti computes hash value of the key and faces the collisions

--- PartA_Task1.py
+++ PartA_Task1.py
@@ -11,3 +11,15 @@ class HashTable:
     def _hash(self, key):
         return hash(key) % self.size
 
+
+    def insert(self, key, value, name):
+        index = self._hash(key)
+        if self.table[index] is None:
+            self.table[index] = [(key, value, name)]
+        else:
+            for i, (k, v, n) in enumerate(self.table[index]):
+                if k == key:
+                    self.table[index][i] = (key, value, name)
+                    break
+            else:
+                self.table[index].append((key, value, name))
