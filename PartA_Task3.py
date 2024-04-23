d4caa38fe65be4e2a17cdca5b989e4b920f0b18f
SaraKhalidAlAli <202206310@zu.ac.ae>
2024-04-24 00:21:34 +0400

    importing heap and defining a class for a Max Heap and initializing

--- PartA_Task3.py
+++ PartA_Task3.py
@@ -1,1 +1,18 @@
 #TASK 3
+
+import heapq
+
+class MaxHeap:
+    def __init__(self):
+        self.heap = []
+
+    def insert(self, post):
+        heapq.heappush(self.heap, (-post['views'], post))
+
+    def get_max_views_post(self):
+        if self.heap:
+            return self.heap[0][1]  # return the post with the highest number of views
+        else:
+            return None
+
+
